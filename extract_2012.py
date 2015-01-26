e4e52d1d579ee38ef5170dfd5c2714231bdf82bd
secretLJW <secretljw@gmail.com>
2015-01-27 01:33:12 +0900

    애니 이미지 추가

--- extract_2012.py
+++ extract_2012.py
@@ -3,14 +3,18 @@ import sys
 import re
 import pickle
 import json
-from translate import db
-from translate.models import *
+import os
+from translate import app
+#from translate import db
+#from translate.models import *
 
 if __name__ == '__main__':
 	data = pickle.load(open("save_dict.p", "rb"))
 	date_list = pickle.load(open("date_list.p", "rb"))
 
 	new_anime_list = []
+	default_dir = os.path.join(app.root_path, 'static/img/')
+	find_dir = '/Users/garden/Desktop/study/python/animoa/translate/static/img/anime'
 
 	for (num, metadata) in data.items():
 		flag = False
@@ -21,6 +25,19 @@ if __name__ == '__main__':
 				if '2012' <= i:
 					flag = True
 		if flag == True:
+			img_path = os.path.join(default_dir, str(num))
+			if not os.path.exists(img_path):
+				os.makedirs(img_path)
+				os.makedirs(os.path.join(img_path, 'characters'))
+			if os.path.exists(os.path.join(find_dir, str(num) + '.jpg')):
+				src_file = open(os.path.join(find_dir, str(num) + '.jpg'))
+				stream = src_file.read()
+				src_file.close()
+				dst_file = open(img_path + '/' + str(num) + '.jpg', "wb")
+				dst_file.write(stream)
+				dst_file.close()
+				print num
+			"""
 			trailer = Trailer(id=int(num), is_translate=0)
 			db.session.add(trailer)
 			db.session.commit()
@@ -34,6 +51,7 @@ if __name__ == '__main__':
 			print "done {0}".format(ani_name)
 			#new_anime_list.append(tuple((num, metadata['ani_name'], metadata['precision'])))
 			#print num
+			"""
 	print "2012년 이후작 추출 완료"
 	
 	#print new_anime_list
